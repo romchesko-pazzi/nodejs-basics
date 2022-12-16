bc82088e6ec5fa48799ab248755132cb9207715a
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-12-16 22:45:17 +0300

    refactor: add flag to prevent rewrite data in file

--- src/streams/write.js
+++ src/streams/write.js
@@ -6,7 +6,7 @@ import { pipeline } from 'stream/promises'
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const fileToWrite = path.join(__dirname, 'files/fileToWrite.txt');
 
-const writableStream = createWriteStream(fileToWrite);
+const writableStream = createWriteStream(fileToWrite, {flags: 'a'});
 
 const write = async () => {
     await pipeline(process.stdin, writableStream);
